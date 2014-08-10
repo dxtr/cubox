8b3aa5cf566a73ee05d86e4546ed652711066b7a
Kim Lidström <kim@dxtr.im>
2014-08-10 13:23:46 +0200

    Updated boot.txt and buildkernel.sh

--- buildkernel.sh
+++ buildkernel.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
 
-# Compile the kernel, embed the DTB and create an initrd, put everything in
+# Compile the kernel and embed the DTB, put everything in
 # /boot
 
-# TODO: Explore the possibility to us zImage instead of uImage
+# TODO: Explore the possibility to use zImage instead of uImage
 
 version=`make kernelversion`
+dtb="dove-cubox.dtb"
 
-make uImage
+cd /usr/src/linux-$version
+
+echo "Building uImage and $dtb"
+make uImage $dtb
+
+echo "Making backup of zImage"
 cp arch/arm/boot/zImage arch/arm/boot/zImage.orig
-make dtbs
-cat arch/arm/boot/zImage.orig arch/arm/boot/dts/dove-cubox.dtb > arch/arm/boot/zImage
+
+echo "Combining zImage and $dtb"
+cat arch/arm/boot/zImage.orig arch/arm/boot/dts/$dtb > arch/arm/boot/zImage
+
+echo "Making uImage"
 make uImage
+
+echo "Making modules"
 make modules
+
+echo "Installing moodules"
 sudo make modules_install
+
+echo "Copying uImage to /boot/uImage-$version"
 sudo cp arch/arm/boot/uImage /boot/uImage-$version
-sudo /usr/sbin/update-initramfs -k $version -c
+#sudo /usr/sbin/update-initramfs -k $version -c
 
